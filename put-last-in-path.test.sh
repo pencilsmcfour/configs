c789b7792bb62c4edd49570bb9af1d132efb4d0b
Douglas Naphas <douglasnaphas@gmail.com>
2021-12-05 19:56:33 -0500

    Add a test for spaces in path
    gh-9

--- put-last-in-path.test.sh
+++ put-last-in-path.test.sh
@@ -5,7 +5,7 @@ run_test() {
   path="$1"
   putlast="$2"
   expected="$3"
-  actual="$(put-last-in-path ${path} ${putlast})"
+  actual=$(put-last-in-path "${path}" "${putlast}")
   if [[ "${expected}" != "${actual}" ]]
   then
     echo "expected ${expected}"
@@ -20,6 +20,6 @@ run_test "a:b:c" "b" "a:c:b"
 run_test "/Users/douglasnaphas/.nvm/versions/node/v16.13.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/douglasnaphas/.nvm/versions/node/v12.22.7/bin" \
         "/usr/bin" \
         "/Users/douglasnaphas/.nvm/versions/node/v16.13.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/bin:/usr/sbin:/sbin:/Users/douglasnaphas/.nvm/versions/node/v12.22.7/bin:/usr/bin"
-# spaces in path
+run_test "a:b  c:d" "b  c" "a:d:b  c"
 
 
